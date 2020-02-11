91377951ea00e139dfb299a6f9f0d04875cdff0c
Darby Dixon III <ddixon@oberlin.edu>
2020-02-11 11:30:31 -0500

    Updating newUpdate script to make life easier.

--- newUpdate.js
+++ newUpdate.js
@@ -1,22 +1,73 @@
 const moment = require('moment');
 const fs = require('fs');
+const prompt = require('prompt');
 
 const now = moment();
 
-const title = process.argv[2] || now.format("YYYY_MM_DD_HH_mm_ss");
+let title = process.argv[2] || now.format("YYYY_MM_DD_HH_mm_ss");
 const created = now.format();
 
-let output = `---
+
+var schema = {
+  properties: {
+    title: {
+      pattern: /^[a-zA-Z\s\-]+$/,
+      message: 'Name must be only letters, spaces, or dashes',
+      required: false
+    },
+    type: {
+      required: true,
+      default: 'blog-page',
+      pattern: /^today-i-learned|blog-page|link-of-note$/,
+      message: 'Type must be today-i-learned, blog-page, or link-of-note'
+    }
+  }
+};
+
+//
+// Start the prompt
+//
+prompt.start();
+
+//
+// Get two properties from the user: email, password
+//
+prompt.get(schema, function (err, result) {
+  //
+  // Log the results.
+  //
+  console.log('Command-line input received:');
+  console.log('  title: ' + result.title);
+  console.log('  type: ' + result.type);
+
+  if (result.title) {
+    title = result.title;
+  }
+
+  let outputDir = "./pages/posts/";
+
+  if (result.type === "link-of-note") {
+    outputDir += "link/";
+  } else if (result.type === "today-i-learned") {
+    outputDir += "til/";
+  };
+
+  let output = `---
 title: ${title}
 date: ${created}
+tags:
+  - ${result.type}
 ---
 
 `;
 
-fs.writeFile("./pages/posts/" + title + ".md", output, function(err) {
-  if(err) {
-    return console.log(err);
-  }
+  fs.writeFile(outputDir + title + ".md", output, function(err) {
+    if(err) {
+      return console.log(err);
+    }
 
-  console.log("Update file generated: ", title);
+    console.log("Update file generated: ", title);
+  });
 });
+
+
