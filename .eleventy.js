69751a0cd798a42b2654a69f5fdac5741ba149fa
Darby Dixon III <ddixon@oberlin.edu>
2022-09-20 14:47:18 -0400

    TZ Fix?

--- .eleventy.js
+++ .eleventy.js
@@ -18,7 +18,7 @@ module.exports = function(eleventyConfig) {
 
   // Foil the trackers by reducing times to their hours, ishyily
   eleventyConfig.addShortcode("myIshyDate", function(date, page) {
-    let now = Moment.parseZone(date);
+    let now = Moment.parseZone(date).utc(true);
 
     let formattedDate = `${now.format("YYYY-MM-DD")}, ~${now.format("h a")}-ish`;
     return formattedDate;
